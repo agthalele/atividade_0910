1bb0fd257f0ea4322c8a7ca50f6c4a6c74531897
agthalele <agatha.leticia.327@gmail.com>
2024-10-09 11:05:46 -0300

    juk.

---
+++ src/main/java/com/agathanae/controller/CadrastoAluno.java
@@ -0,0 +1,26 @@
+package com.agathanae.controller;
+import com.gluonhq.charm.glisten.control.AutoCompleteTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class CadsrastoAluno {
+
+    @FXML
+    private AutoCompleteTextField<?> cpf;
+
+    @FXML
+    private AutoCompleteTextField<?> email;
+
+    @FXML
+    private AutoCompleteTextField<?> nascimento;
+
+    @FXML
+    private AutoCompleteTextField<?> nome;
+
+    @FXML
+    void cadrastarAluno(ActionEvent event) {
+
+    }
+
+}
